2d055f8802508331b0f1877c29f1bc5b8980321d
abdoulayegk <balde9745@outlook.com>
2022-03-08 21:51:16 +0530

    initial commit

---
+++ evaluateReversePolishNotation.py
@@ -0,0 +1,41 @@
+"""Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
+
+Note that division between two integers should truncate toward zero.
+
+It is guaranteed that the given RPN expression is always valid. That means the expression would always evaluate to a result, and there will not be any division by zero operation.
+
+
+
+Example 1:
+
+Input: tokens = ["2","1","+","3","*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+Example 2:
+
+Input: tokens = ["4","13","5","/","+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6"""
+
+from typing import List
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for i in tokens:
+            if i == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif i == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif i == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif i == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b / a))
+            else:
+                stack.append(int(i))
+        return stack[0]
